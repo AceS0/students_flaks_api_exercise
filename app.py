1431a4b0d5e90e36c1bbadd2771c229b42e80ea4
AceS0 <abdulcelilsekerci@gmail.com>
2026-02-10 10:50:19 +0100

    Requirements added

--- app.py
+++ app.py
@@ -7,10 +7,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-@app.before_first_request
-def create_table():
-    db.create_all()
-
 # Create a new student
 @app.route('/students', methods=['POST'])
 def add_student():
@@ -52,4 +48,6 @@ def delete_student(id):
     return jsonify({'message': 'Student deleted successfully'}), 200
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
